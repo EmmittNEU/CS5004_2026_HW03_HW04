b14a82eaffe277043725855147e542009bfe1847
Emmitt Tucker <ejtfrogman@gmail.com>
2026-02-03 16:15:58 -0500

    Updated TimeCard.java error

--- src/main/java/student/TimeCard.java
+++ src/main/java/student/TimeCard.java
@@ -1,13 +1,29 @@
 package student;
 
+/**
+ * An implementation of ITimeCard.
+ */
 public class TimeCard implements ITimeCard {
+    private String employeeID;
+    private double hoursWorked;
+
+    /**
+     * A constructor for the TimeCard class.
+     * @param employeeID the ID of the employee.
+     * @param hoursWorked the number of hours worked by the employee.
+     */
+    public TimeCard(String employeeID, double hoursWorked) {
+        this.employeeID = employeeID;
+        this.hoursWorked = hoursWorked;
+    }
+
     @Override
     public String getEmployeeID() {
-        return "";
+        return employeeID;
     }
 
     @Override
     public double getHoursWorked() {
-        return 0;
+        return hoursWorked;
     }
 }
